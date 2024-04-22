f0f936baa7bdbd5d9ded3352d26c785115ec7aab
denis <anywaymaybe1@gmail.com>
2024-04-22 21:45:59 +0400

    main: add babel-loader

--- frontend/config/setting/buildLoaders.ts
+++ frontend/config/setting/buildLoaders.ts
@@ -31,8 +31,29 @@ export const buildLoaders = (options: BuildOptions_T): ModuleOptions["rules"] =>
     ],
   }
 
+  const babelLoader = {
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [
+          '@babel/preset-env',
+          "@babel/preset-typescript",
+          [
+            "@babel/preset-react",
+            {
+              runtime: isDev ? 'automatic' : 'classic',
+            }
+          ]
+        ]
+      }
+    }
+  }
+
   return [
     scssLoader,
-    tsLoader
+    // tsLoader,
+    babelLoader
   ]
 }
